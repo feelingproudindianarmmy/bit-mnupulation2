09036d8dd7eadc31ffbdac13d4ece966bde50593
feelingproudindianarmy <celestialh4@gmail.com>
2024-10-10 17:55:59 +0545

    factorial commit

---
+++ bitplay3/factorial number.py
@@ -0,0 +1,7 @@
+#factorial using recursion
+def fac(n):
+    if n==0 or n==1:
+        return 1
+    return n*fac(n-1)
+n=int(input("enter the number"))
+print(fac(n))
---
+++ bitplay3/practise of poer set .py
@@ -0,0 +1,22 @@
+#how to ask input from user
+num=int(input("Enter an erray"))
+
+#how to define any funtions like power set
+def printpowerset(set,setsize):
+
+#how to take some loops
+for i in range(0,num):
+    num2=int(input("enter element"))
+
+#how to call the funtion
+printpowerset(set,len(set))
+
+#how to import any library(maths)
+import math;
+
+#nested loops
+
+for i in range(0,num):
+    for j in range(1,num):
+
+#this things are very much neccasary or esantial  to understand and aply also..
